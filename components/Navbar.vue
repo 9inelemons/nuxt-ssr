39a5ec777a5787e7470cc881e8cce42c3ec757ce
Vladislav Kosarev <v.kosarev@qsoft.ru>
2020-12-09 21:39:56 +0600

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Nuxt SSR</a>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <nuxt-link exact active-class="active" class="nav-link" to="/">
+                            Home
+                        </nuxt-link>
+                    </li>
+                    <li class="nav-item">
+                        <nuxt-link active-class="active" class="nav-link" to="/about">
+                            About
+                        </nuxt-link>
+                    </li>
+                    <li class="nav-item">
+                        <nuxt-link active-class="active" class="nav-link" to="/users">
+                            Users
+                        </nuxt-link>
+                    </li>
+                    <li class="nav-item" v-if="!hasToken">
+                        <nuxt-link active-class="active" class="nav-link" to="/Login">
+                            Login
+                        </nuxt-link>
+                    </li>
+                    <li class="nav-item" v-else>
+                        <nuxt-link to="#">
+                            <a class="nav-link" href="#" @click.prevent="logout">Logout</a>
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+    computed: {
+        hasToken() {
+            return this.$store.getters.hasToken
+        }
+    },
+
+    methods: {
+        logout() {
+            this.$store.dispatch('logout')
+            this.$router.push('/')
+        }
+    }
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <h1>About Page</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequuntur cupiditate doloribus fuga quae quidem sequi temporibus tenetur, vitae voluptatem!</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cupiditate ducimus ea impedit possimus reprehenderit.</p>
+
+
+    </section>
+</template>
+
+<script>
+export default {
+    middleware: ['auth']
+}
+</script>
